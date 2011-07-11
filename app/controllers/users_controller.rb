8825ce3e5e81cb48d52f6c18c31e8534b8116b56
Jan-Hettich <janhettich@gmail.com>
2011-07-10 19:47:09 -0700

    fadded Users index page with pagination (Section 10.3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all.paginate(:page => params[:page], :per_page => 5)
+    @title = "AllUsers"
+  end
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(:page => params[:page])
