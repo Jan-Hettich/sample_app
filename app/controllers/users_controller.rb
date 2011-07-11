307975225b495f95f2b145832b17c2d55b212a4f
Jan-Hettich <janhettich@gmail.com>
2011-07-10 18:30:45 -0700

    edit page (Sections 10.1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -19,6 +24,17 @@ class UsersController < ApplicationController
     else
       @title = "Sign up"
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render :edit
     end
   end
 
