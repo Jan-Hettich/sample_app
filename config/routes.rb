a5612f41975f49dfc7afcfcc7c3d040f9373162b
Jan-Hettich <janhettich@gmail.com>
2011-07-17 17:56:05 -0700

    missed out some files

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ SampleApp::Application.routes.draw do
   resources :users
   match '/signup', :to => 'users#new'
 
+  resources :microposts, :only => [:create, :destroy]
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
